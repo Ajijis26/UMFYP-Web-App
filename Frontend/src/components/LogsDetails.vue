4aa4b341ad3027b604d0911cc6d37b6b3d0221ec
MOHAMAD AZIZI BIN MOHD SAYUTI <92780417+Ajijis26@users.noreply.github.com>
2025-01-06 03:55:26 +0800

    Change button refresh icon & edit css for btn pagination

--- Frontend/src/components/LogsDetails.vue
+++ Frontend/src/components/LogsDetails.vue
@@ -6,7 +6,7 @@
       <div class="action-buttons">
         <!-- Refresh Button -->
         <button class="btn-refresh" @click="fetchLogs">
-          <i class="fas fa-sync-alt"></i> Refresh
+          <i class="bi bi-arrow-clockwise"></i>
         </button>
 
         <!-- Attribute Selection -->
@@ -748,6 +748,13 @@ export default {
 .btn-next[disabled] {
   background-color: gray;
   cursor: not-allowed;
+}
+
+.btn-prev:hover:not([disabled]),
+.btn-next:hover:not([disabled]) {
+  background-color: #0056b3;
+  transform: scale(1.05);
+  transition: all 0.3s ease;
 }
 
 .modal-overlay {
