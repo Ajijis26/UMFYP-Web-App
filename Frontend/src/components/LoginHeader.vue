9226898a9d4273867e265550e9f72272f5bb363b
MOHAMAD AZIZI BIN MOHD SAYUTI <92780417+Ajijis26@users.noreply.github.com>
2025-01-05 12:06:17 +0800

    Follow css style of General Header

--- Frontend/src/components/LoginHeader.vue
+++ Frontend/src/components/LoginHeader.vue
@@ -10,15 +10,14 @@
     display: flex;
     justify-content: left;
     align-items: center;
-    padding: 0.5em; /* Adjust the padding */
+    padding: 0.5em 1em; /* Adjust the padding */
     background-color: #000000; /* White with 80% opacity */
     color: #FFFFFF;
     font-size: 14px; /* Adjust the font size */
     font-weight: bold;
     border-top: 3px solid #000080;
     border-bottom: 1px solid #FFE140;
-    margin-bottom: 0em;
-    height: 60px; /* Adjust the height of the header */
+    height: 100%; /* Adjust the height of the header */
 }
 
 .logo {
@@ -29,9 +28,11 @@
     transform: translateX(-20px);
     animation: slideIn 0.8s forwards;
 }
+
 .logo:hover {
     transform: scale(1.1); /* Slight zoom */
 }
+
 .header-title {
   margin-left: 10px;
   margin-right: auto;
