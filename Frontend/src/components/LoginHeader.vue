e9b827e0d7f68992107b3913019c047fae5fa603
MOHAMAD AZIZI BIN MOHD SAYUTI <92780417+Ajijis26@users.noreply.github.com>
2025-01-10 18:30:33 +0800

    Remove Logo from Header

--- Frontend/src/components/LoginHeader.vue
+++ Frontend/src/components/LoginHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="login-header">
-        <img class="logo" src="/src/assets/Logo UM.png" alt="Logo">
+        <!--img class="logo" src="/src/assets/Logo UM.png" alt="Logo"-->
         <h1 class="header-title">IDS Web Application</h1>
     </header>
 </template>
